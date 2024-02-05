9bfb949850eb54619c9f61675932f4938811702c
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-02-05 11:48:44 -0300

    .

--- public/sw.js
+++ public/sw.js
@@ -69,7 +69,7 @@ if (!self.define) {
 }
 define(['./workbox-e43f5367'], (function (workbox) { 'use strict';
 
-  importScripts();
+  importScripts("worker-development.js");
   self.skipWaiting();
   workbox.clientsClaim();
   workbox.registerRoute("/", new workbox.NetworkFirst({
